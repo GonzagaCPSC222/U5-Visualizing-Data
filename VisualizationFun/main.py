3228fb4ee5a6a441f45525a398acad6368258ae6
Gina Sprint <sprint@gonzaga.edu>
2022-03-01 14:51:08 -0800

    finish line example

--- VisualizationFun/main.py
+++ VisualizationFun/main.py
@@ -1,4 +1,35 @@
 import pandas as pd
+import matplotlib.pyplot as plt 
+
+def line_chart_example(x_ser, y_ser, y2_ser):
+    plt.plot(x_ser, y_ser, label="PTS", lw=5, ls="--")
+    plt.plot(x_ser, y2_ser, label="MIN")
+
+    # lets beautify the plot
+    # lets the overlapping x tick labels
+    plt.xticks(rotation=25, ha="right")
+    plt.xlabel("Players")
+    plt.ylabel("Total")
+    plt.title("Gonzaga 2021-22 Season Total")
+    plt.grid()
+    plt.legend()
+    # task: add another parameter to our line_chart_example function
+    # for the total minutes played
+    # and add a line to the chart for total minutes played
+    # are points and minutes played related?
+    
+    # right before "rending" the figure, good to call
+    plt.tight_layout()
+    # 3 ways to "render" a figure
+    # 1. plt.show()
+    # create a modal window to interact with the figure
+    # plt.show()
+    # 2. plt.savefig(path)
+    # save the figure to a file (major file types are 
+    # supported... png, jpg, pdf, ...)
+    plt.savefig("line_example.png")
+    # 3. inline inside a jupyter notebook (stay tuned...)
+
 
 def main():
     df =  pd.read_csv("bball.csv", index_col=0)
@@ -24,5 +55,37 @@ def main():
     print(ppg_ser)
     # lots of ways to do this...
     print(ppg_ser[ppg_ser == ppg_ser.max()])
+
+    # EDA: exploratory data analysis
+    # getting familiar with your data
+    # summarizing it using stats, visualizing it, 
+    # mining it for patterns, relationships, groups, etc.
+
+    # goals of data visualization
+    # 1. clearly and accurately represent data
+    # 2. be creative, with the goal of increasing readability
+    # and understanding
+    # 3. label axes, units, and points of interest
+
+    # some jargon
+    # chart: 2D visualization
+    # plot: a chart of data points (e.g. scatter plot)
+    # graph: a chart of a math function (e.g. sine curve)
+
+    # we are going to use the matplotlib library for our plotting
+    # a few ways to use this
+    # 1. pyplot module 
+    # there is always a "current" figure
+    # 2. OOP (object oriented programming) interface
+    # 3. a mix of the #1 and #2
+    # we will do #1, and drop down to #3 if we have to
+
+    # let's start with a simple line chart
+    # we will show players on the x-axis 
+    # and PTS on the y-axis
+    # note: line chart is not the best way to display
+    # continuous data (PTS) grouped by discrete values (players)
+    line_chart_example(df.index, df["PTS"], df["MIN"])
+    
 
 main()
